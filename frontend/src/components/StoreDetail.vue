9d5bc5c83b6179a61e4f1eb21c50b46f72305e8b
hnjang <ha-neul@naver.com>
2022-09-21 17:45:10 +0900

    오타수정 및 menus 데이터 타입 변경

--- frontend/src/components/StoreDetail.vue
+++ frontend/src/components/StoreDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="storeList">
         <div v-for="(menu, index) in menus" :key="index">
-            {{index + 1}}. {{menu.menuName}}
+          {{ index + 1 }}.{{ menu.menuName }}
         </div>
         <div style="margin-top: 30px;">
             <router-link to="/store">BACK</router-link>
@@ -9,23 +9,23 @@
     </div>
 </template>
   
-  <script>
-    export default {
-      data() {
-        return {
-          menus: [],
-        }
-      },
-      created: function() {
-        var id = this.$route.params.id;
-        this.$http.get(`/api/store/${id}`)
-        .then((response) => {
-          this.menus = response.data[0]
-        })
+<script>
+  export default {
+    data() {
+      return {
+        menus: {},
       }
-    };
-  </script>
-  
-  <style>
-  
-  </style>
+    },
+    created: function() {
+      var id = this.$route.params.id;
+      this.$http.get(`/api/stores/${id}`)
+      .then((response) => {
+        this.menus = response.data
+      })
+    }
+  };
+</script>
+
+<style>
+
+</style>
