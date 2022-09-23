58469945e860be171f2d206bab02f53301821fba
hnjang <ha-neul@naver.com>
2022-09-23 17:27:24 +0900

    리뷰 등록하기 추가

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -36,4 +36,23 @@ router.get('/:id', function (req, res) {
   });
 });
 
+router.post('/create', function (req, res) {
+  var id = parseInt(req.params.id, 10)
+  const form = {
+    'username': req.body.form.username,
+    'storeid': req.body.form.storeid,
+    'star': req.body.form.star,
+    'content': req.body.form.content,
+  };
+
+  connection.query("INSERT INTO review (username, store_id, star, content) VALUES ('" + form.username + "', '"+ form.storeid +"', '"+ form.star  +"', '" + form.content + "')", function (err, rows) {
+    if (err) throw err;
+    res.json({
+      success: true,
+      message: '등록되었습니다.'
+    })
+  });
+});
+
+
 module.exports = router;
--- frontend/src/components/ReviewDetail.vue
+++ frontend/src/components/ReviewDetail.vue
@@ -1,16 +1,31 @@
 <template>
     <div class="reviewDetail">
-        <div v-for="(review, index) in reviews" :key="index">
-          {{index + 1}}. {{users[review.user_id].userid}}의 평가 : {{review.content}}
+        <div>가게이름 추가할것</div>
+        <div v-if="reviews.length == 0">
+            아직 이 가게의 리뷰가 1도 없습니다.
+        </div>
+        <div v-for="(review, idx) in reviews" :key="idx">
+          {{idx + 1}}. {{review.username}}의 평가 : {{review.content}} (
+            <span v-for="idx in review.star" :key="idx">🧡</span>
+          )
         </div>
         <div class="newReview">
             <hr>
-            <div class="title"><h3>새로운 리뷰를 아래에 작성하세요.</h3></div>
+            <div class="title"><h3>이 가게의 새로운 리뷰를 아래에 작성하세요.</h3></div>
             <label>아이디
-                <input type="text" name="userid" value=""/>
+                <input type="text" name="userid" v-model="form.username"/>
             </label>
-            <textarea style="margin-top:20px" placeholder="음식의 맛, 가격, 웨이팅 여부 등"></textarea>
-            <button class="primary">리뷰 올리기</button>
+            <label> 별점
+                <select v-model="form.star">
+                    <option :value="1">🧡🤍🤍🤍🤍</option>
+                    <option :value="2">🧡🧡🤍🤍🤍</option>
+                    <option :value="3">🧡🧡🧡🤍🤍</option>
+                    <option :value="4">🧡🧡🧡🧡🤍</option>
+                    <option :value="5">🧡🧡🧡🧡🧡</option>
+                </select>
+            </label>
+            <textarea style="margin-top:20px" placeholder="음식의 맛, 가격, 웨이팅 여부 등" v-model="form.content"></textarea>
+            <button class="primary" @click="create">리뷰 올리기</button>
         </div>
         <div style="margin-top: 30px;">
             <router-link to="/store">BACK</router-link>
@@ -20,22 +35,41 @@
 
 <script>
 export default {
+    methods: {
+        create: function (event) {
+            var id = this.$route.params.id;
+            this.form.storeid = id
+            this.$http.post("/api/reviews/create", {
+                form: this.form
+            })
+                .then((res) => {
+                    if (res.data.success == true) {
+                        alert(res.data.message);
+                        this.$router.go();
+                    }
+                })
+                .catch(function (error) {
+                    alert("error");
+                });
+        }
+    },
     data() {
         return {
           reviews: {},
-          users: {},
+          form: {
+            username: '',
+            storeid: '',
+            star: '1',
+            content: ''
+          }
         }
       },
     created: function () {
         var id = this.$route.params.id;
         this.$http.get(`/api/reviews/${id}`)
-            .then((response) => {
-                this.reviews = response.data
+            .then((res) => {
+                this.reviews = res.data
             })
-
-        this.$http.get("/api/users").then((response) => {
-            this.users = response.data;
-        });
     }
 };
 </script>
