09409133f7ed85c8c7cd22eba4421dd7371f93ee
hnjang <ha-neul@naver.com>
2022-09-28 15:12:44 +0900

    별점 표시 오류 수정

--- frontend/src/components/StoreDetail.vue
+++ frontend/src/components/StoreDetail.vue
@@ -10,7 +10,7 @@
         <div v-for="(review, idx) in reviews">
           {{idx + 1}}. {{review.username}}의 평가 : {{review.content}} (
             <span v-for="cnt in 5">
-                {{cnt < review.star ? '🧡' : '🤍'}}
+                {{cnt <= review.star ? '🧡' : '🤍'}}
             </span>
           )<button @click="modiReview(idx)">수정.</button><button @click="delReview">삭제.</button>
         </div>
@@ -29,7 +29,6 @@ export default {
         modiReview: function(idx){
             var id = this.$route.params.id;
             var num = this.reviews[idx].review_id;
-            console.log("clicked modiReview")
             this.$router.push({name: 'reviewModify', params: {id, num}})
         },
         delReview: function() {
