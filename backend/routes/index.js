6d36a18b6f6578ae10e145fe3c2731f6dd7fb903
hnjang <ha-neul@naver.com>
2022-09-28 17:38:40 +0900

    메인화면 빌드된 화면으로 보이게 수정

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+
+  // res.render('index', { title: 'Express' }); // 기존 파일
 });
 
 module.exports = router;
